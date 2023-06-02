a58a601999e9860006aeb3ecdd4bc0ea48bc0404
VadikRakitin01 <vadym.rakitin@gmail.com>
2023-06-02 12:19:53 +0300

    TEST

--- js/questions.js
+++ js/questions.js
@@ -1,70 +1,113 @@
 // створення масиву та передача чисел, питань, варіантів та відповідей
 let questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+  numb: 1,
+  question: "Що означає термін HTML?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+{
+  numb: 2,
+  question: "Який тег служить для розміщення коду JavaScript:",
+  answer: "SCRIPT",
+  options: [
+   "META",
+   "HEAD",
+   "STYLE",
+   "SCRIPT"
+   ]
+},
+  {
+  numb: 3,
+  question: "Що означає термін PHP?",
+  answer: "Hypertext Preprocessor",
+  options: [
+    "Hypertext Preprocessor",
+    "Hypertext Programming",
+    "Hypertext Preprogramming",
+    "Hometext Preprocessor"
+  ]
+},
+  {
+  numb: 4,
+  question: "Що означає термін SQL?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+{
+  numb: 5,
+  question: "Що з переліченого не є мовою програмування?",
+  answer: "MySql",
+  options: [
+   "Java",
+   "MySql",
+   "Python",
+   "Swift"
+   ]
+},
+{
+  numb: 6,
+  question: "Що означає термін XML?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+{
+  numb: 7,
+  question: "Регулярні вирази у мові PHP використовують:",
+  answer: "пошуку заданих шаблонів у даних стрічкового типу",
+  options: [
+   "для організації циклів",
+   "для встановлення cookies",
+   "пошуку заданих шаблонів у даних стрічкового типу",
+   "для доступу до даних HTML форм"
+   ]
+ },
+{
+  numb: 8,
+  question: "Що означає термін CSS?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+{
+  numb: 9,
+  question: "Функція mysql_connect()",
+  answer: "встановлює з'єднання із сервером MySQL",
+  options: [
+   "звільняє памʼять",
+   "встановлює з'єднання із сервером MySQL",
+   "відправляє серверу MySQL запит",
+   "закриває з'єднання із сервером MySQL"
+   ]
+},
+{
+  numb: 10,
+  question: "Індекси масиву у PHP-коді записують у",
+  answer: "[]",
+  options: [
+   "META",
+   "[]",
+   "(**)",
+   "{}"
+   ]
+},
 ];
